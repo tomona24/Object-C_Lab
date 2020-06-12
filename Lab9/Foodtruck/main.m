b0aff0894226dcbbdf64c7ceaa313c9544062147
Tomona Sako <sakotomona@gmail.com>
2020-06-11 23:07:12 -0700

    Finish Lab9

---
+++ Lab9/Foodtruck/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  FoodTruck
+//
+//  Copyright © 2016 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FoodTruck.h"
+#import "Cook.h"
+#import "Cook2.h"
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        FoodTruck *truckA = [[FoodTruck alloc] initWithName:@"Take a Bao" andFoodType:@"bao"];
+        
+        FoodTruck *truckB = [[FoodTruck alloc] initWithName:@"Tim Shortons" andFoodType:@"shortbread"];
+        
+        Cook2 *cookA = [[Cook2 alloc]init];
+        Cook2 *cookB = [[Cook2 alloc]init];
+        truckA.delegate = cookA;
+        truckB.delegate = cookB;
+        // create instances of your delegate class
+        // set truckA and truckB's delegate to your new instance.
+        
+        [truckA serve:10];
+        [truckB serve:5];
+        
+        [truckA cashOut];
+        [truckB cashOut];
+    }
+    return 0;
+}
